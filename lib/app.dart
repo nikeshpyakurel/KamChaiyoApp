3147b712e9cbffeda7a62873d499504da2f9fa5c
Nikesh Pyakurel <nikeshpyakurel32@gmail.com>
2025-05-17 11:29:00 +0545

    Main Fille Assets
    App and main assets

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:kamchaiyo/view/splash_Screen.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
+  }
+}
