f857b35c0925fa3d84b2b1a3d7ef6385932b7113
Nikesh Pyakurel <nikeshpyakurel32@gmail.com>
2025-06-19 16:01:22 +0545

    Theme Constant
    Created the theme constant for the project

---
+++ lib/app/constant/theme_constant.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ThemeConstant {
+  ThemeConstant._();
+
+  static const Color primaryColor = Color(0xFF7F98F3);
+  static const Color lightPrimaryColor = Color.fromARGB(255, 127, 152, 243);
+  static const Color scaffoldBackgroundColor = Colors.white;
+  static const Color surfaceColor = Color(0xFFF3F6FD);
+  static const Color primaryTextColor = Colors.black87;
+  static const Color secondaryTextColor = Colors.grey;
+  static const Color textOnPrimaryColor = Colors.white;
+  static const Color errorColor = Colors.redAccent;
+  static const Color successColor = Colors.green;
+  static const String fontFamilyPhilosopher = 'Philosopher Bold';
+  static const String fontFamilyNunito = 'Nunito Regular';
+  static const String fontFamilyNunitoBold = 'Nunito Bold';
+  static const double borderRadius = 10.0;
+}
