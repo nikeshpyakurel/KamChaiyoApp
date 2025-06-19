62073187cc2d1554581fadb54e57c9e9ae12d80f
Nikesh Pyakurel <nikeshpyakurel32@gmail.com>
2025-06-19 16:03:01 +0545

    SnackBar
    Moved the my_snackBar to the core/common folder

---
+++ lib/core/common/snackbar/my_snackbar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void showSnackBar({
+  required BuildContext context,
+  required String message,
+  Color? color,
+}) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(message),
+      backgroundColor: color ?? Colors.black,
+      behavior: SnackBarBehavior.floating,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      margin: const EdgeInsets.all(16),
+    ),
+  );
+}
