e56db833b5e275c0e9b7955a4a0ebcae7de700a3
Nikesh Pyakurel <nikeshpyakurel32@gmail.com>
2025-06-19 16:11:39 +0545

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:kamchaiyo/app.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kamchaiyo/app/service_locator/service_locator.dart';
+import 'package:kamchaiyo/app/theme/app_theme.dart';
+import 'package:kamchaiyo/features/auth/presentation/view_model/auth_view_model.dart';
+import 'package:kamchaiyo/features/splash/presentation/view/splash_view.dart';
 
-void main() {
-  runApp(App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupLocator();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => sl<AuthViewModel>()..add(AuthCheckRequested()),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'KamChaiyo',
+        theme: AppTheme.getApplicationTheme(),
+        home: const SplashView(),
+      ),
+    );
+  }
 }
