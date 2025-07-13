85a67b64cab445dfbbaba6a0df9bbe6df5eafe00
Nikesh Pyakurel <nikeshpyakurel32@gmail.com>
2025-07-13 19:20:31 +0545

    Create auth_repo.mock.dart
    Created the replica of auth repo for mock test

---
+++ test/features/auth/domain/use_case/auth_repo.mock.dart
@@ -0,0 +1,6 @@
+import 'package:kamchaiyo/features/auth/domain/repository/auth_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class AuthRepoMock extends Mock implements AuthRepository{
+  
+}
